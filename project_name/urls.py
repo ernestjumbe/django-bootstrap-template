f099e9b2a3692b3ca7742dd3822a3fd6aaa83c93
ernestjumbe <e2jeyy@gmail.com>
2016-06-13 00:52:03 +0200

    Django 1.9 settings

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,18 +1,18 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.views.generic.base import TemplateView
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', TemplateView.as_view(template_name="index.html"), {}, name='index'),
 
     url(r'^admin/', include(admin.site.urls)),
-)
+]
 
 if settings.DEBUG and settings.MEDIA_ROOT:
     urlpatterns += static(settings.MEDIA_URL,
@@ -20,6 +20,6 @@ if settings.DEBUG and settings.MEDIA_ROOT:
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns += patterns('',
+    urlpatterns += [
         url(r'^__debug__/', include(debug_toolbar.urls)),
-    )
+    ]
